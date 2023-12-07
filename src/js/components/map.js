ef74f09328ac5478a20df5be861ad5ed66e43fb4
beji <simone0443@outlook.com>
2023-12-07 18:56:02 +0200

    icon goes brr

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -1,7 +1,39 @@
 console.log("hello world from map.js");
+
 var map = L.map('map', { zoomControl: false }).setView([46.186504598160354, 21.31672212393795], 11);
-console.log(L.DomUtil);
+
 import { baseURL } from "../index";
+
+uwuArray = [];
+
+let tramIcon = L.icon({
+    iconUrl: require("../../img/victorious.png"),
+
+    iconSize:     [50, 50], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [5, 5], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+
+});
+
+let tram = L.marker([46.192717706185675, 21.30671085657869], {icon: tramIcon}).addTo(map);
+
+
+function updateLocation()
+{
+    if(uwuArray.length > 0)
+    {
+        console.log(uwuArray);
+
+        let latitude = uwuArray[0].latitude;
+        let longitude = uwuArray[0].longitude;
+
+        let newLatLng = new L.LatLng(latitude, longitude);
+        
+        tram.setLatLng(newLatLng); 
+    }
+}
 
 function uwurequest() {
 
@@ -10,9 +42,15 @@ function uwurequest() {
         origin: "*"
     })
     .then(response => response.json())
-    .then(body => console.log(body.vehicles[0]));
+    .then(body => 
+        {
+            //console.log(body.vehicles);
+            uwuArray = body.vehicles;
+        });
 
+    updateLocation();
 }
+
 
 setInterval(uwurequest, 1000);
 
