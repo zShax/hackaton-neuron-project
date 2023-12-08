e141ea7409f7eacf5ad36caef5db0dd2bcf820e7
beji <simone0443@outlook.com>
2023-12-08 19:31:56 +0200

    multiple points

--- src/js/components/side-menu-button.js
+++ src/js/components/side-menu-button.js
@@ -1,16 +1,16 @@
+import { map } from '../index';
+
 let hideShowSideMenu = () => {
+  let sideButton = document.getElementById('side-menu-button');
+  var sideMenu = document.getElementById('side-menu');
 
-    let sideButton = document.getElementById("side-menu-button");
-    var sideMenu = document.getElementById("side-menu");
+  if (sideMenu.style.display === 'block') {
+    sideMenu.style.display = 'none';
+    sideButton.style.left = '0%';
+  } else {
+    sideMenu.style.display = 'block';
+    sideButton.style.left = '57%';
+  }
+};
 
-    if (sideMenu.style.display === "block") {
-        sideMenu.style.display = "none";
-        sideButton.style.left = "0%";
-    } else {
-        sideMenu.style.display = "block";
-        sideButton.style.left = "57%";
-    }
-}
-
-document.getElementById("side-menu-button").onclick = hideShowSideMenu;
-
+document.getElementById('side-menu-button').onclick = hideShowSideMenu;
